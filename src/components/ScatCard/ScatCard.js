5555f09ef1ad1b77adcf05f589741d31f30c590e
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-07-09 15:45:19 -0600

    something something something

--- src/components/ScatCard/ScatCard.js
+++ src/components/ScatCard/ScatCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
 import scatShape from '../../helpers/propz/scatShape';
 
 class ScatCard extends React.Component {
@@ -16,19 +18,20 @@ class ScatCard extends React.Component {
 
   render() {
     const { scat } = this.props;
+    const singleLink = `/scat/${scat.id}`;
     return (
-      <div className = "ScatCard col-4">
-      <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{scat.sampleNum}</h5>
-        <p className="card-text">{scat.location}</p>
-        <a href="#" className="btn btn-danger" onClick={this.deleteMe}>Delete</a>
+      <div className="ScatCard col-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{scat.sampleName}</h5>
+            <Link className="btn btn-success" to={singleLink}>View</Link>
+            <p className="card-text">{scat.location}</p>
+            <button className="btn btn-danger" onClick={this.deleteMe}>Delete</button>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     );
   }
 }
-
 
 export default ScatCard;
